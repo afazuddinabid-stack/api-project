672af2f025c6e4d2ebd37cb5a979cff15a29b4d5
afazuddinabid-stack <afazuddinabid@gmail.com>
2026-03-08 12:09:47 +0600

    creted sign in function

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Hello, World!");
+ document.getElementById("signin-btn").addEventListener("click", function() {
+    const inputuser = document.getElementById("input-user")
+    const inputpassword = document.getElementById("input-password")
+    const username = inputuser.value;
+    const password = inputpassword.value;
+    console.log(username, password);
+    if (username === "admin" && password === "admin123") {
+        alert("Login successful!");
+        window.location.href = "home.html";
+    }
+    else{
+        alert("Login failed! Please check your username and password.");
+        return false;
+    }
+ });
